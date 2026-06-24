3503d73f4509e11447634803bba9e775cf3d8a02
Darpan-n-Jain <darpan1206@gmail.com>
2026-06-24 16:52:56 +0530

    modules

--- hello.js
+++ hello.js
@@ -1,1 +1,9 @@
-console.log("hello world")
+const add = require('./add.js');
+const math= require('./math.js');
+//const {add, sub}= require('./math.js'); //to use func as console.log(sub(1 ,2))
+
+
+console.log("hello world");
+console.log(add(2,8)); 
+console.log(math.add(5 ,10));
+console.log(math.subfn(5 ,10));
