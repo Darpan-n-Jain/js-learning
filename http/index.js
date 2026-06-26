749e8d3be691d8df119fbd8f5aa81c3d8466b8f3
Darpan-n-Jain <darpan1206@gmail.com>
2026-06-27 00:07:05 +0530

    url and http server handling methods

--- http/index.js
+++ http/index.js
@@ -1,17 +1,31 @@
 const http = require("http");
 const fs = require("fs")
+const url = require("url");
 
 const myserver = http.createServer((req, res) =>{
-    const log =`${Date.now()}: ${req.url} new req recived \n`;
+    if (req.url === "/favicon.ico") return res.end();
+    const log =`${Date.now()}: ${req.method} ${req.url} new req recived \n`;
+        const myurl = url.parse(req.url, true);
+        console.log(myurl);
     fs.appendFile("log.txt", log ,(err , data) => {
     // res.end("hello fom darpan");
-    switch (req.url) {
+    switch (myurl.pathname) {
         case "/":
-            res.end("home page");
+            if (runInNewContext.method === 'GET') req.end("Home page")
             break;
         case "/about":
-        res.end("I am darpan jain");
+            const username = myurl.query.myname
+        res.end(`Hi, ${username}`);
         break;
+        case "/search" :
+        const search = myurl.query.search_queary;
+        res.end("here is result for" + search);
+        case '/singup':
+            if (req.method === "GET") res.end("this is singup form");
+            else if (req.method === "POST"){
+                //db query
+                res.end("success");
+            }
         default:
             res.end("eror404");
             break;
